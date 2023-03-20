68eb0a69fa5c9ddd3fa2c588902d2047f5a17a99
Hugues Soares <huguescarlos.soares@gmail.com>
2023-03-20 17:09:31 -0400

    add cors lib

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express, { Application, NextFunction, Request, Response }  from "express";
+import cors from "cors";
 
 const app: Application = express();
 const port: number = 3000;
+
+app.use(cors())
 
 app.use('/', (req: Request, res: Response, next: NextFunction): void => {
   res.send('Hello world!');
